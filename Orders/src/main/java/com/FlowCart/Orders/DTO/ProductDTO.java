de8cf40708157dfb94e7acb7f4469379932c24c3
Divyansh Mathur <divyanshmathur1804@gmail.com>
2026-04-22 22:09:29 +0530

    feat: docker-setup

---
+++ Orders/src/main/java/com/FlowCart/Orders/DTO/ProductDTO.java
@@ -0,0 +1,34 @@
+package com.FlowCart.Orders.DTO;
+
+public class ProductDTO {
+     private Long id;
+     private String name;
+     private Double price;
+     private Integer stock;
+     public Long getId() {
+         return id;
+     }
+     public void setId(Long id) {
+         this.id = id;
+     }
+     public String getName() {
+         return name;
+     }
+     public void setName(String name) {
+         this.name = name;
+     }
+     public Double getPrice() {
+         return price;
+     }
+     public void setPrice(Double price) {
+         this.price = price;
+     }
+     public Integer getStock() {
+         return stock;
+     }
+     public void setStock(Integer stock) {
+         this.stock = stock;
+     }
+
+     
+}
--- Orders/src/main/java/com/FlowCart/Orders/Service/OrderServices.java
+++ Orders/src/main/java/com/FlowCart/Orders/Service/OrderServices.java
@@ -3,8 +3,12 @@ package com.FlowCart.Orders.Service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.FlowCart.Orders.DTO.ProductDTO;
 import com.FlowCart.Orders.Entity.Orders;
 import com.FlowCart.Orders.Repository.OrdersRepository;
 
@@ -13,16 +17,41 @@ import jakarta.transaction.Transactional;
 @Service
 public class OrderServices {
     private OrdersRepository ordersRepository;
+    
+    @Value("${product.service.url}")
+    private String productServiceUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public OrderServices(OrdersRepository ordersRepository) {
         this.ordersRepository = ordersRepository;
     }
 
+    public ProductDTO getProduct(Integer productId) {
+    String url = productServiceUrl + "/" + productId;
+    return restTemplate.getForObject(url, ProductDTO.class);
+}
+
     @Transactional
     public Orders createOrder(Orders order) {
-        return ordersRepository.save(order);
+        ProductDTO product = getProduct(order.getProductId());
+
+    if (product == null) {
+        throw new RuntimeException("Product not found");
     }
+
+    if (product.getStock() < order.getQuantity()) {
+        throw new RuntimeException("Out of stock");
+    }
+
+    return ordersRepository.save(order);
+    }
+
     public Optional<Orders> getOrderById(Integer orderId) {
+
+
+    
         return ordersRepository.findById(orderId);
     }
 
@@ -37,6 +66,13 @@ public class OrderServices {
     @Transactional
     public void updateOrderQuantity(Integer orderId, Integer quantity) {
         Orders order = ordersRepository.findById(orderId).orElseThrow(() -> new RuntimeException("Order not found"));
+        ProductDTO product = getProduct(order.getProductId());
+        if (product == null) {
+            throw new RuntimeException("Product not found");
+        }
+        if (product.getStock() < quantity) {
+            throw new RuntimeException("Out of stock");
+        }
         order.setQuantity(quantity);
         ordersRepository.save(order);
     }
